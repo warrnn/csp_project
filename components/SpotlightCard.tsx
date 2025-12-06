c8afa538d3eaeeed8fd146c029b84e8865b68f41
Bryan Warren <bryanwarren86@gmail.com>
2025-12-06 15:08:37 +0700

    fixing card

--- components/SpotlightCard.tsx
+++ components/SpotlightCard.tsx
@@ -53,7 +53,7 @@ const SpotlightCard: React.FC<SpotlightCardProps> = ({
       onBlur={handleBlur}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`relative rounded-3xl border border-neutral-800 bg-neutral-900 overflow-hidden ${className}`}
+      className={`relative rounded-3xl overflow-hidden ${className}`}
     >
       <div
         className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out"
